ad15ef4070fac667bd74d776c89d44e61ca64586
Rushad Antia <rushadantia@gmail.com>
2018-06-21 11:08:08 -0400

    after 3 days i got it to do what i want

--- 3d_plotting/script.js
+++ 3d_plotting/script.js
@@ -1,18 +1,8 @@
- var pointCount = 300;
- var i, r;
 
  var x = [];
  var y = [];
  var z = [];
  var c = [];
- 
- for (i = 0; i < pointCount; i++) {
-     r = i * (pointCount - i);
-     x.push(r * Math.cos(i / 30));
-     y.push(r * Math.sin(i / 30));
-     z.push(i);
-     c.push(i)
- }
 
  Plotly.plot('graph', [{
      type: 'scatter3d',
@@ -30,22 +20,26 @@
 
  setInterval(() => {
 
- var xx = [];
- var yy = [];
- var zz = [];
- 
- for (i = 0; i < 100; i++) {
-     r = i * (pointCount - i);
-     x.push(r * Math.cos(i / 30)*Math.floor(Math.random()*9));
-     y.push(r * Math.sin(i / 30)*Math.floor(Math.random()*9));
-     z.push(i*Math.floor(Math.random()*9));
- 
- }
 
- Plotly.extendTraces('graph', {
-    x: [xx],
-    y: [yy],
-    z: [zz]
-  }, [0], x.length)
+     x.push(Math.floor(Math.random() * 29));
+     y.push(Math.floor(Math.random() * 99));
+     z.push(Math.floor(Math.random() * 9));
+     c.push(Math.floor(Math.random() * 255));
 
- }, 1000);
+
+     Plotly.react('graph', [{
+         type: 'scatter3d',
+         mode: 'lines',
+         x: x,
+         y: y,
+         z: z,
+         opacity: 0.7,
+         line: {
+             width: 5,
+             color: c,
+             colorscale: 'Viridis'
+         }
+     }]);
+
+
+ }, 50);
